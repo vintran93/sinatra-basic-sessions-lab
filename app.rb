5792f2d2a5dc72d933c71ebe6356d57b6358109a
vincent <vincentuong@yahoo.com>
2021-09-05 19:03:58 -0500

    commit tree 5dbc4d6630b94994553f28544d15ec5d988d90fd

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, 'Amy'
+    end
+
+    get '/' do
+        erb :index
+    end
+    
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkoout
+    end
 end
